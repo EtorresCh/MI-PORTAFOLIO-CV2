9d3df498e97f3b81d65325f7d568cd7a3cc77dd7
EDGARJTORRES <edgar7352253@gmail.com>
2025-03-16 22:54:31 -0500

    mejoro el nav- y agrego mas animaciones

--- JS/index.js
+++ JS/index.js
@@ -2,21 +2,51 @@ window.sr = ScrollReveal();
 sr.reveal('.navbar',{
        duration: 2000,
        origin: 'bottom',
-       distance: '-100px'
+       distance: '-100px',
+       viewFactor: 0.01,
     });
+sr.reveal('.portada',{
+   duration: 2000,
+      origin: 'left',
+      distance: '-150px',
+      viewFactor: 0.01,
+   });
 
 sr.reveal('.card',{
       duration: 2000,
       origin: 'top',
-      distance: '-300px'
+      distance: '-300px',
+      viewFactor: 0.01,
    });
-sr.reveal('.skills',{
+sr.reveal('.barra1',{
+      duration: 2000,
+      origin: 'left',
+      distance: '-300px',
+      viewFactor: 0.01,
+   });
+
+   sr.reveal('.barra2',{
+      duration: 2000,
+      origin: 'right',
+      distance: '-300px',
+      viewFactor: 0.01,
+   });
+
+sr.reveal('.adaptable1',{
+      duration: 2000,
+      origin: 'left',
+      distance: '300px',
+      viewFactor: 0.01,
+   });   
+sr.reveal('.adaptable2',{
       duration: 2000,
       origin: 'top',
-      distance: '-300px'
+      distance: '-300px',
+      viewFactor: 0.01,
    });
-sr.reveal('.adaptable',{
+sr.reveal('.adaptable3',{
       duration: 2000,
-      origin: 'top',
-      distance: '-300px'
+      origin: 'right',
+      distance: '300px',
+      viewFactor: 0.01,
    });
