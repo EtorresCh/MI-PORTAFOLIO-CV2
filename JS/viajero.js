4da40a936a0912abf6a523aeb1131b8c79233fb2
EDGARJTORRES <edgar7352253@gmail.com>
2025-03-23 00:00:14 -0500

    agrego nueva ventana aruma

---
+++ JS/viajero.js
@@ -0,0 +1,21 @@
+window.sr = ScrollReveal();
+sr.reveal('.navbar',{
+       duration: 2000,
+       origin: 'bottom',
+       distance: '-100px',
+       viewFactor: 0.01,
+    });
+sr.reveal('.img-viajero',{
+        duration: 2000,
+        origin: 'left',
+        distance: '300px',
+        viewFactor: 0.01,
+     });
+  
+sr.reveal('.info-viajero',{
+        duration: 2000,
+        origin: 'right',
+        distance: '300px',
+        viewFactor: 0.01,
+     });
+  
